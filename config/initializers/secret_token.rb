cbd080a8cc318e8fa19cfd06b93dd3fb2deb13b7
Helmut Fleps <helmut_fleps@yahoo.com>
2014-08-13 10:42:10 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wacker::Application.config.secret_key_base = 'ceb56a1e9597a73d3dcbc038314654cb61c5deda77e62e8e8b0024e731ba9644a1d7b40558282e87954efb0da9e17c347c8a21f4411de334afeb7d18b020b629'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wacker::Application.config.session_store :cookie_store, key: '_wacker_session'
